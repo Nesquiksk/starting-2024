ac610851709df5d3977c931c7811f8283ede5fab
Nesquiksk <samko0601@gmail.com>
2024-09-27 07:57:44 +0200

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Typography from '@mui/material/Typography';
+
+export default function Home() {
+  return (
+    <Typography variant="h5" gutterBottom>
+    Very simple website :3
+    </Typography>
+  )
+}
