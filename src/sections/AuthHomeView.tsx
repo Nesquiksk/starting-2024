c6353db555729cfbcb3431d6d0dc8adf7c6c70bc
Nesquiksk <samko0601@gmail.com>
2025-01-10 08:27:21 +0100

    AuthGuard added

--- src/sections/AuthHomeView.tsx
+++ src/sections/AuthHomeView.tsx
@@ -1,26 +1,21 @@
 // src/sections/AuthHomeView.tsx
 
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-// import Box from "@mui/material/Box";
-
+import React from "react";
 import { Session } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function AuthHomeView({ session }: { session: Session }) {
-
-  return (
-    <Container>
-      <Typography> Domovská stránka - prihlásený user</Typography>
-      <Typography variant="h4" sx={{ mb: 3 }}>
-        Vitajte, {session?.user?.name || "užívateľ"}!
-      </Typography>
-
-
-      {/* <Box sx={{ mt: 2 }}>
-        <pre>{JSON.stringify(session, null, 2)}</pre>
-      </Box> */}
-    </Container>
-  );
+interface AuthHomeViewProps {
+  session: Session;
 }
 
+const AuthHomeView: React.FC<AuthHomeViewProps> = ({ session }) => {
+  // Redirect if the user is authenticated
+  if (session) {
+    redirect("/prispevok");
+    return null; // Avoid rendering anything after redirect
+  }
 
+  return null; // Alternatively, render fallback UI if needed
+};
+
+export default AuthHomeView;
